5e33d5cd11e0a4edee8c7fe3b3c4b1416e26470a
didar775 <d_alikhan@kbtu.kz>
2023-02-26 00:48:11 +0600

    main

---
+++ FirstApp/src/app/products.ts
@@ -0,0 +1,32 @@
+export interface Product{
+    id:number;
+    name:string;
+    price:string;
+    link:string;
+    image:string;
+}
+
+
+export let productss:Product[] = [
+    {
+        id:1,
+    name:"ea",
+    price:'145',
+    link: 'asfdaf',
+    image:'fdasfdas'
+    },
+    {
+        id:1,
+    name:"ea",
+    price:'145',
+    link: 'asfdaf',
+    image:'fdasfdas'
+    },
+    {
+        id:1,
+    name:"ea",
+    price:'145',
+    link: 'asfdaf',
+    image:'fdasfdas'
+    }
+]
